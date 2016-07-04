a3c1bf438551cdc47075190ff61ed5f097496e80
Hishiv Shah <shahh14@cardiff.ac.uk>
2016-07-04 15:32:39 +0100

    Added WFD Coastal Waterbodies polygons.

---
+++ code/coastalWaters.py
@@ -0,0 +1,39 @@
+import os
+
+import ogr
+import psycopg2
+
+# Input paths
+projectDir = "D:\Projects\coastalHeatCapacity"
+dataDir = os.path.join(projectDir, "data")
+gmlPath = os.path.join(dataDir, "nrw",
+                       "WaterFrameworkDirectiveCoastalWaterbodiesCycle2.gml")
+
+# Connect to postgis database
+connStr = "dbname=coastalHeat user=postgres password=ownwardenter"
+with psycopg2.connect(connStr) as db:
+    cur = db.cursor()
+
+    # Create table
+    cur.execute("DROP TABLE IF EXISTS coastalWaters;")
+    cur.execute("""CREATE TABLE coastalWaters (
+                       wb_id TEXT PRIMARY KEY,
+                       name TEXT,
+                       geometry GEOMETRY(MULTIPOLYGON, 27700)
+                    );""")
+
+    # Read GML file
+    dataSource = ogr.Open(gmlPath)
+    layer = dataSource.GetLayer()
+    layer.SetAttributeFilter("EA_REG_CD = 8")
+    for feature in layer:
+        wb_id = feature.GetField("EA_WB_ID")
+        name = feature.GetField("NAME")
+        geometry = feature.GetGeometryRef()
+        wkt = geometry.ExportToWkt()
+
+        # Insert into table
+        cur.execute("""INSERT INTO coastalWaters
+                       VALUES (%s, %s,
+                               ST_Multi(ST_GeomFromText(%s, 27700)));""",
+                    (wb_id, name, wkt))
--- data/readme.txt
+++ data/readme.txt
@@ -12,3 +12,8 @@ nasa:
 - 4km resolution
 - Source: http://oceancolor.gsfc.nasa.gov/cgi/l3
 - Format: HDF Standard Mapped Image File
+
+nrw:
+- Water Framework Directive Coastal Water Bodies Cycle 2
+- source: http://lle.wales.gov.uk/catalogue/item/WaterFrameworkDirectiveCoastalWaterbodiesCycle2/?lang=en
+- format: gml
